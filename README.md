b01fa8e0a6c4e14d4df87254ba340c4d3af2e175
Jason Park <jason808@naver.com>
2025-01-23 15:24:08 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# timer_game
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/timer_game/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.timer_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
